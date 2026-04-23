092b6d03fe0abe73b1b4020555935eb84487ca28
Keith Owino <designsolutions1629@gmail.com>
2026-04-23 14:27:12 +0300

    Init commit.

---
+++ public/firebase.config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+	apiKey: "AIzaSyC2bogoDFQgVUSLJpTCdA-vybqtgiChaiQ",
+	authDomain: "codemylife-d8330.firebaseapp.com",
+	projectId: "codemylife-d8330",
+	storageBucket: "codemylife-d8330.firebasestorage.app",
+	messagingSenderId: "240224270822",
+	appId: "1:240224270822:web:072725aaeb2bcf36d63e27",
+};
+
+firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
